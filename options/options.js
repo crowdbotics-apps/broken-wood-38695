f9f2c3fbbc3fbb24d5f851d53ad941bbe2d70805
Crowdbotics <team@crowdbotics.com>
2023-02-03 21:47:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-wood-38695",
+  url: "https://broken-wood-38695.botics.co",
+  api: "https://broken-wood-38695.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
